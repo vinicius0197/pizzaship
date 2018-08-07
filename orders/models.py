1ab6cc10be3b652d8957e7f91e67b5b7fe962300
Vinicius <viniciuscosta0197@gmail.com>
2018-08-07 11:54:30 -0300

    Added pricing model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Pricing(models.Model):
+    topping = models.IntegerField()
+    small_price = models.DecimalField(max_digits=5, decimal_places=2)
+    large_price = models.DecimalField(max_digits=5, decimal_places=2)
+    type = models.BooleanField()
