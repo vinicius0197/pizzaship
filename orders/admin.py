6de4549ce875692894e77c42e2776b08c11d0476
Vinicius <viniciuscosta0197@gmail.com>
2018-08-07 21:45:32 -0300

    Added Topping and Order model

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Pricing, Topping, Order
 
 # Register your models here.
+admin.site.register(Pricing)
+admin.site.register(Topping)
+admin.site.register(Order)
--- orders/models.py
+++ orders/models.py
@@ -2,7 +2,25 @@ from django.db import models
 
 # Create your models here.
 class Pricing(models.Model):
-    topping = models.IntegerField()
+    numberToppings = models.IntegerField()
     small_price = models.DecimalField(max_digits=5, decimal_places=2)
     large_price = models.DecimalField(max_digits=5, decimal_places=2)
     type = models.BooleanField()
+
+    def __str__(self):
+        return "Number of toppings: " + str(self.numberToppings)
+
+class Topping(models.Model):
+    topping = models.CharField(max_length=64)
+
+    def __str__(self):
+        return str(self.topping)
+
+class Order(models.Model):
+    size = models.IntegerField()
+    numberToppings = models.IntegerField()
+    type = models.BooleanField()
+    toppings = models.ManyToManyField(Topping, blank=True, related_name="orders")
+
+    def __str__(self):
+        return "Order: " + str(self.size)
