b7c99b6a34a0a6602e105c5981ecd27f74b1f9fb
Vinicius <viniciuscosta0197@gmail.com>
2018-08-07 22:24:26 -0300

    Fixed fields for Pricing and Order models

--- orders/models.py
+++ orders/models.py
@@ -2,13 +2,37 @@ from django.db import models
 
 # Create your models here.
 class Pricing(models.Model):
-    numberToppings = models.IntegerField()
+    # Define types of toppings for pizza
+    CHEESE = 'CHEESE'
+    T1 = '1'
+    T2 = '2'
+    T3 = '3'
+    T4 = 'SPECIAL'
+
+    TOPPING_TYPES = (
+        (CHEESE, 'Cheese'),
+        (T1, '1 topping'),
+        (T2, '2 toppings'),
+        (T3, '3 toppings'),
+        (T4, 'Special'),
+    )
+    number_toppings = models.CharField(max_length=10,
+                                        choices=TOPPING_TYPES,
+                                        default=CHEESE)
     small_price = models.DecimalField(max_digits=5, decimal_places=2)
     large_price = models.DecimalField(max_digits=5, decimal_places=2)
-    type = models.BooleanField()
 
+    REGULAR = 'R'
+    SICILIAN = 'S'
+    PIZZA_TYPES = (
+        (REGULAR, 'Regular'),
+        (SICILIAN, 'Sicilian'),
+    )
+    type = models.CharField(max_length=2,
+                            choices=PIZZA_TYPES,
+                            default=REGULAR)
     def __str__(self):
-        return "Number of toppings: " + str(self.numberToppings)
+        return "Number of toppings: " + str(self.number_toppings)
 
 class Topping(models.Model):
     topping = models.CharField(max_length=64)
@@ -17,10 +41,43 @@ class Topping(models.Model):
         return str(self.topping)
 
 class Order(models.Model):
-    size = models.IntegerField()
-    numberToppings = models.IntegerField()
-    type = models.BooleanField()
+    CHEESE = 'CHEESE'
+    T1 = '1'
+    T2 = '2'
+    T3 = '3'
+    T4 = 'SPECIAL'
+
+    TOPPING_TYPES = (
+        (CHEESE, 'Cheese'),
+        (T1, '1 topping'),
+        (T2, '2 toppings'),
+        (T3, '3 toppings'),
+        (T4, 'Special'),
+    )
+    number_toppings = models.CharField(max_length=10,
+                                        choices=TOPPING_TYPES,
+                                        default=CHEESE)
+    LARGE = 'L'
+    SMALL = 'S'
+    SIZE_OPTIONS = (
+        (LARGE, 'Large'),
+        (SMALL, 'Small'),
+    )
+
+    size = models.CharField(max_length=1,
+                            choices=SIZE_OPTIONS,
+                            default=SMALL)
+    REGULAR = 'R'
+    SICILIAN = 'S'
+    PIZZA_TYPES = (
+        (REGULAR, 'Regular'),
+        (SICILIAN, 'Sicilian'),
+    )
+    type = models.CharField(max_length=2,
+                            choices=PIZZA_TYPES,
+                            default='Regular')
     toppings = models.ManyToManyField(Topping, blank=True, related_name="orders")
+    final_price = models.DecimalField(max_digits=5, decimal_places=2, default=0)
 
     def __str__(self):
         return "Order: " + str(self.size)
